e7810153e6905e41ddb8fe6a9af555c48cc957c1
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-09-23 14:32:08 -0600

    Create Boot MVC Project

---
+++ OutTheDoor/src/main/java/com/skilldistillery/otd/OutTheDoorApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.otd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutTheDoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutTheDoorApplication.class, args);
+	}
+
+}
---
+++ OutTheDoor/src/test/java/com/skilldistillery/otd/OutTheDoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.otd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutTheDoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
