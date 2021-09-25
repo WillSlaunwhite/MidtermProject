b32bb99aad25373eec715eb9b0188e9e6daf0406
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-09-25 11:48:05 -0600

    added fields to Category and Rating

--- JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Category.java
+++ JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Category.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.otd.entities;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
@@ -8,4 +9,35 @@ public class Category {
 	@GeneratedValue
 	private int id;
 
+	@Column(name="activity_type")
+	private String activity;
+	
+	
+	
+	public Category() {}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getActivity() {
+		return activity;
+	}
+
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Category [id=").append(id).append(", activity=").append(activity).append("]");
+		return builder.toString();
+	}
 }
--- JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Rating.java
+++ JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Rating.java
@@ -1,11 +1,53 @@
 package com.skilldistillery.otd.entities;
 
-import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
 import javax.persistence.Id;
 
 public class Rating {
-	@Id
-	@GeneratedValue
-	private int id;
+//	 Not entirely sure the correct way to do two primary ids for rating,
+//	 so leaving these placeholders here for now until we map relationship properly.
+	
+//	@Id
+//	@Column(name="activity_id")
+//	private int userId;	
+	
+//	@Id
+//	@Column(name="activity_id")
+//	private int activityId;
+	
+	private int rating;
+	
+	@Column(name="rate_comment")
+	private String ratingComment;
+	
+	
+	
+	public Rating() {}
+	
+	
 
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getRatingComment() {
+		return ratingComment;
+	}
+
+	public void setRatingComment(String ratingComment) {
+		this.ratingComment = ratingComment;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Rating [rating=").append(rating).append(", ratingComment=").append(ratingComment).append("]");
+		return builder.toString();
+	}
+	
+	
 }
