cd1173465c320805f88cadf129310df1c4e2b238
cgardner90 <calebgardner2015@gmail.com>
2021-09-25 17:55:02 -0600

    Caleb Committing framed out ACTIVITY and IMAGE entities

--- JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Activity.java
+++ JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Activity.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.otd.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,4 +10,39 @@ public class Activity {
 	@Id
 	@GeneratedValue
 	private int id;
+	
+	private String title;
+	
+	@Column(name="pro_tips")
+	private String proTips;
+	
+	@Column(name="elevation_gain_feet")
+	private int elevationGain;
+
+	private String route;
+	
+	@Column(name="handicap_accessible")
+	private boolean handicapAccessible;
+	
+	@Column(name="dogs_allowed")
+	private boolean dogsAllowed;
+	
+	private boolean parking;
+	
+	private String description;
+	
+	@Column(name="profile_url")
+	private String profileURL;
+	
+	@Column(name="category_id")
+	private int categoryId;
+	
+	@Column(name="location_id")
+	private int locationId;
+	
+	private int difficulty;
+	
+	
+	
+	
 }
--- JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Image.java
+++ JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Image.java
@@ -1,11 +1,28 @@
 package com.skilldistillery.otd.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Image {
 	@Id
 	@GeneratedValue
 	private int id;
+	
+	@Column(name="image_url")
+	private String imageURL;
+	
+	@Column(name="user_id")
+	private int stringId;
+	
+	@Column(name="activity_id")
+	private int activityId;
+	
+	@Column(name="date_added")
+	private LocalDate dateAdded;
 
 }
