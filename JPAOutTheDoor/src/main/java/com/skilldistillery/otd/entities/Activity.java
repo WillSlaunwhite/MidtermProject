35453cfa5dae8664a686c42de9f412d95ca21f88
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-09-28 17:37:16 -0600

    saving myself

--- JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Activity.java
+++ JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Activity.java
@@ -1,12 +1,253 @@
 package com.skilldistillery.otd.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Activity {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+
+	private String title;
+
+	@Column(name = "pro_tips")
+	private String proTips;
+
+	@Column(name = "elevation_gain_feet")
+	private int elevationGain;
+
+	private String route;
+
+	@Column(name = "handicap_accessible")
+	private boolean handicapAccessible;
+
+	@Column(name = "dogs_allowed")
+	private boolean dogsAllowed;
+
+	private boolean parking;
+
+	private String description;
+
+	@Column(name = "profile_url")
+	private String profileURL;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToOne
+	@JoinColumn(name = "location_id")
+	private Location location;
+
+	private int difficulty;
+
+	@Column(name = "distance_miles")
+	private double distanceInMiles;
+
+	
+	@OneToMany(mappedBy="activity")
+	private List<Image> images;
+	
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+	@OneToMany(mappedBy="activity")
+	private List<Comment> comments;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getProTips() {
+		return proTips;
+	}
+
+	public void setProTips(String proTips) {
+		this.proTips = proTips;
+	}
+
+	public int getElevationGain() {
+		return elevationGain;
+	}
+
+	public void setElevationGain(int elevationGain) {
+		this.elevationGain = elevationGain;
+	}
+
+	public String getRoute() {
+		return route;
+	}
+
+	public void setRoute(String route) {
+		this.route = route;
+	}
+
+	public boolean isHandicapAccessible() {
+		return handicapAccessible;
+	}
+
+	public void setHandicapAccessible(boolean handicapAccessible) {
+		this.handicapAccessible = handicapAccessible;
+	}
+
+	public boolean isDogsAllowed() {
+		return dogsAllowed;
+	}
+
+	public void setDogsAllowed(boolean dogsAllowed) {
+		this.dogsAllowed = dogsAllowed;
+	}
+
+	public boolean isParking() {
+		return parking;
+	}
+
+	public void setParking(boolean parking) {
+		this.parking = parking;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getProfileURL() {
+		return profileURL;
+	}
+
+	public void setProfileURL(String profileURL) {
+		this.profileURL = profileURL;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public int getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public double getDistanceInMiles() {
+		return distanceInMiles;
+	}
+
+	public void setDistanceInMiles(double distanceInMiles) {
+		this.distanceInMiles = distanceInMiles;
+	}
+	
+	public void addImage(Image image) {
+		if(images == null) images = new ArrayList<>();
+		
+		if(!images.contains(image)) {
+			images.add(image);
+			if(image.getActivity() !=null) {
+				image.getActivity().getImages().remove(image);
+			}
+			image.setActivity(this);
+		}
+	}
+	
+	public void removeImage(Image image) {
+		image.setActivity(null);
+		if(image !=null) {
+			images.remove(image);
+		}
+	}
+	public void addComment(Comment comment) {
+		if(comments == null) comments = new ArrayList<>();
+		
+		if(!comments.contains(comment)) {
+			comments.add(comment);
+			if(comment.getActivity() !=null) {
+				comment.getActivity().getComments().remove(comment);
+			}
+			comment.setActivity(this);
+		}
+	}
+	
+	public void removeComment(Comment comment) {
+		comment.setActivity(null);
+		if(comment !=null) {
+			comments.remove(comment);
+		}
+	}
+	
+	
+	
+	
+	
+
+	public List<Image> getImages() {
+		return images;
+	}
+
+	public void setImages(List<Image> images) {
+		this.images = images;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Activity other = (Activity) obj;
+		return id == other.id;
+	}
+
 }
--- OutTheDoor/src/main/java/com/skilldistillery/otd/controllers/HomeController.java
+++ OutTheDoor/src/main/java/com/skilldistillery/otd/controllers/HomeController.java
@@ -1,17 +1,25 @@
 package com.skilldistillery.otd.controllers;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.skilldistillery.otd.data.SearchDao;
 import com.skilldistillery.otd.data.UserDao;
+import com.skilldistillery.otd.entities.Activity;
 
 @Controller
 public class HomeController {
 	@Autowired
 	private UserDao userDao;
-	
+	@Autowired
+	private SearchDao searchDao;
+
 	
 	@RequestMapping(path = {"/", "home.do"} )
 	public String home(Model model) { 
@@ -19,6 +27,45 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(path="getAllHiking.do", method=RequestMethod.GET)
+	public String allHikes(Model model) {
+		model.addAttribute("results", searchDao.findAllHiking());
+		return "allHikes";
+		
+	}
 	
+	@RequestMapping(path="getAllBiking.do", method=RequestMethod.GET)
+	public String allBiking(Model model) {
+		model.addAttribute("results", searchDao.findAllBiking());
+		return "allBiking";
+		
+	}
+	
+	@RequestMapping(path="getAllKayaking.do", method=RequestMethod.GET)
+	public String allKayaking(Model model) {
+		model.addAttribute("results", searchDao.findAllKayaking());
+		return "allKayaking";
+		
+	}
+
+	@RequestMapping(path="getAllRafting.do", method=RequestMethod.GET)
+	public String allRafting(Model model) {
+		model.addAttribute("results", searchDao.findAllRafting());
+		return "allRafting";
+		
+	}
+
+	@RequestMapping(path="getAllFishing.do", method=RequestMethod.GET)
+	public String allFishing(Model model) {
+		model.addAttribute("results", searchDao.findAllFishing());
+		return "allFishing";
+		
+	}
+	
+	@RequestMapping(path="getActivity.do", method=RequestMethod.GET)
+	public String getActivity(@RequestParam("id") int id, Model model) {
+		model.addAttribute("activity", searchDao.findActivityById(id));
+		return "activity";
+	}
 	
 }
