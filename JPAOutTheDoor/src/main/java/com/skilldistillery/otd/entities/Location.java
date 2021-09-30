2b630ab924a2b287b998236d93f37858e867ae23
cgardner90 <calebgardner2015@gmail.com>
2021-09-30 16:13:59 -0600

    Add Location functionality worked on

--- JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Location.java
+++ JPAOutTheDoor/src/main/java/com/skilldistillery/otd/entities/Location.java
@@ -6,13 +6,14 @@ import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Location {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	private String street;
